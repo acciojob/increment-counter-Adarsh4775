673b6ff745fdab24b08558637ca95ce3bedc00fc
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-25 16:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function incrementCount(){
+	const count = document.getElementById("counter");
+	const incrementBtn = document.getElementById("incrementBtn");
+	let counter = 0;
+
+	incrementBtn.addEventListener("click", function(){
+		alert('previous value: ${counter}');
+		counter++;
+		 count.textContent = counter;
+	});
+}
+incrementCount();
